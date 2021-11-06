c94dbe08bd2cd2dd7214b20e965dfbafa8278e5a
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-06 08:25:43 +0100

    :sparkles: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
+import {
+  FaChevronUp,
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+} from "react-icons/fa";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <footer id="footer" className="">
+      <Container>
+        <div className=" footer_socials d-flex justify-content-between align-items-center ">
+          <div className="go_to_top  order-lg-3">
+            <button
+              onClick={scrollToTop}
+              className="goUp"
+              aria-label="go to the top"
+            >
+              <FaChevronUp />
+            </button>
+          </div>
+          <span className="lead  text-white my-3">
+            &copy; {new Date().getFullYear()}, MuPro. All rights reserved
+          </span>
+          <div className="socials  d-flex gap-3 align-items-center text-white">
+            <a href="https://github.com/">
+              <FaInstagram className="social_icon" />
+              <span className="visually-hidden">Instagram</span>
+            </a>
+            <a href="https://github.com/">
+              <FaFacebook className="social_icon" />
+              <span className="visually-hidden">Facebook</span>
+            </a>
+            <a href="https://github.com/">
+              <FaTwitter className="social_icon" />
+              <span className="visually-hidden">Twitter</span>
+            </a>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
